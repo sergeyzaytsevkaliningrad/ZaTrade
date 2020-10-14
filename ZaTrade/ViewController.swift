3b9ada0152571df3e129942ad9047e3261f04808
Andrey Popov <a.popov99@mail.ru>
2020-10-14 16:29:18 +0300

    Initial Commit

---
+++ ZaTrade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZaTrade
+//
+//  Created by Андрей Попов on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

4907e57969ba821df77a310cb0a3daaca6faac0c
nansy99 <62338878+nansy99@users.noreply.github.com>
2020-11-08 18:43:53 +0300

    AddProduct

--- ZaTrade/AppDelegate.swift
+++ ZaTrade/AppDelegate.swift
@@ -11,7 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
     
-        let viewController = InfoCountryViewController()
+        let viewController = AddProductViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         
         window.rootViewController = navigationController

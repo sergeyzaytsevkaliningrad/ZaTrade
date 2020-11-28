405ac1090b26885270d743fb653ade30a778a587
Andrey Popov <a.popov99@mail.ru>
2020-11-28 12:01:06 +0300

    Edited WelcomeViewController and WelcomePresenter

--- ZaTrade/Presenter/WelcomePresenter.swift
+++ ZaTrade/Presenter/WelcomePresenter.swift
@@ -1,10 +1,36 @@
 import Foundation
+import UIKit
 
 class WelcomePresenter {
     weak var viewController: WelcomeViewController?
+    let counties: [EntityWrapper<Country>]
+    
+    var currentCountryIndex: Int? {
+        didSet {
+            if currentCountryIndex != nil {
+                self.viewController?.chooseCountry.countryFlag.text = counties[currentCountryIndex!].entity?.flag
+                self.viewController?.chooseCountry.countryTitle.text = counties[currentCountryIndex!].entity?.name
+            }
+        }
+    }
+    
+    init() {
+        self.counties = EntityWrapper<Country>.all(sortKey: "name", ascending: true)
+    }
     
     @objc func openTabBarController() {
         let tabBarController = TabBarController()
         self.viewController!.navigationController?.pushViewController(tabBarController, animated: true)
     }
+    
+    @objc func openCountryPickleView() {
+        self.viewController?.showChooseCountryAlert()
+    }
+    
+    @objc func openTaxFreeInformation() {
+        let informationController = InfoCountryViewController()
+        // TODO: add data to view controller
+        self.viewController!.navigationController?.pushViewController(informationController, animated: true)
+    }
+    
 }
